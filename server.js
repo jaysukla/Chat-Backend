d22f2b13eaa46378d405ac5dc636c7bf9a09c4b8
jaysukla <am7343249@gmail.com>
2023-01-23 13:09:42 +0530

    backend

---
+++ server.js
@@ -0,0 +1,58 @@
+const http = require('http');
+const express = require('express');
+const aap = express();
+const cors = require('cors')
+const server = http.createServer(aap);
+
+// const port = process.env.PORT || 3000
+
+// aap.use(express.static(__dirname+'/public'))
+
+aap.get('/',(req,res)=>{
+      
+res.send("API based Url")
+
+})
+
+
+/*  socket.io setup   */
+
+const io= require('socket.io')(server);
+
+const users= {};
+
+
+
+
+io.on('connection',(socket)=>{
+    
+socket.on('new-user-joined',(username)=>{
+    users[socket.id]=username;
+    // console.log(users)
+socket.broadcast.emit('user-connected',username)
+
+io.emit('user-list',users);
+console.log(users)
+})
+
+socket.on('disconnect',()=>{
+    socket.broadcast.emit('user-disconnected',user=users[socket.id]);
+    delete users[socket.id];
+ 
+    io.emit('user-list',users)
+})
+
+
+socket.on('message',(data)=>{
+    socket.broadcast.emit('massage',data)
+})
+
+
+})
+
+
+
+server.listen(3000,()=>{
+    console.log("Listning to [port 3000]")
+})
+
